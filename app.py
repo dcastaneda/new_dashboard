2b8fb4b0219165511a67f3f82b89fad57e488aab
Daniel Castañeda <dcastagneda@gmail.com>
2022-07-04 22:58:35 -0500

    adding project

--- app.py
+++ app.py
@@ -99,4 +99,4 @@ server = app.server
 
 # Testing server, don't use in production, host
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8050, debug=True)
+    app.run_server(debug=True)
